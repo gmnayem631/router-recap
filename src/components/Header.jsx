ced50f5838f3021c294ab3a580641972123ede39
gmnayem631 <gmnayem631@gmail.com>
2026-03-12 20:59:51 +0600

    nested route recap

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h3 className="text-3xl">This is header</h3>
+    </div>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Component, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./components/Root.jsx";
+import Home from "./components/Home.jsx";
+import Mobiles from "./components/Mobiles.jsx";
+import Laptops from "./components/Laptops.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/mobiles",
+        Component: Mobiles,
+      },
+      {
+        path: "/laptops",
+        Component: Laptops,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
-)
+);
